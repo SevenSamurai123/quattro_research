ac5542980786fa6ab92605b52a55ef155aad5e65
SevenSamurai123 <c.kloppert@gmx.de>
2025-08-01 17:23:44 +0200

    helper functions

---
+++ Utils.py
@@ -0,0 +1,17 @@
+
+class Utils():
+
+    def print_dictionary(dictionary_of_proteins):
+        for key, value in dictionary_of_proteins.items():
+            print(key, value.get_description(), value.get_seq_reg_name())
+
+    def get_uniprot_informations_of_protein(protein):
+        print(protein.get_accession_number())
+        print(protein.get_protein_name())
+        print(protein.get_gen())
+        print(protein.get_organism_scitific())
+        print(protein.get_organism_common())
+        print(protein.get_mol_weight())
+        print(protein.get_ensembl_gene_id())
+        print(protein.get_description())
+        print(protein.get_seq_reg_name())
